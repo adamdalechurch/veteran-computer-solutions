3249d36ba8fd76bde95ab302f00f9a095c488e41
AdamChurch <38051837+adamdalechurch@users.noreply.github.com>
2024-01-19 18:23:36 -0500

    css breakup :(

--- header.php
+++ header.php
@@ -5,22 +5,43 @@
 <head>
     <meta charset="UTF-8">
     <title>IT Consulting</title>
-    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/base.css">
+    <link rel="stylesheet" href="assets/css/layout.css">
+    <link rel="stylesheet" href="assets/css/navigation.css">
+    <link rel="stylesheet" href="assets/css/subheader.css">
+    <link rel="stylesheet" href="assets/css/utilities.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
     <link rel="stylesheet" href="assets/css/normalize.css">
+    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </head>
 <body>
 <div class="header">
-    <nav class="top-nav">
-        <!-- Navigation links here -->
-        <!-- <a href="#" class="nav-link">Link 1</a> -->
-        <?php foreach ($pages as $link):?>
-            <a href="<?php echo $link['link']; ?>" class="nav-link"><?php echo $link['title']; ?></a>
-        <?php endforeach; ?>
-        <!-- More links -->
-        <button class="menu-button">Menu</button>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">
+                <!-- Add your site logo here -->
+                <img src="/path-to-your-logo.svg" alt="Logo" style="height: 40px;">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <?php foreach ($pages as $link): ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo $link['link']; ?>"><?php echo $link['title']; ?></a>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        </div>
     </nav>
     <div class="subheader">
         <!-- Subheader content -->
+        <div class="container">
+            <h2>Welcome to Our IT Consulting Services</h2>
+            <p>Expert solutions for your business technology needs</p>
+        </div>
     </div>
 </div>
