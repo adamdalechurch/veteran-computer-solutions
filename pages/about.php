c6ff382d65eb21e90fcd07591e0b0031ba6180c8
garylstarr <155030944+garylstarr@users.noreply.github.com>
2024-01-21 14:07:26 -0500

    Update about.php
    added text

--- pages/about.php
+++ pages/about.php
@@ -6,6 +6,8 @@
         <div class="separator"></div>
         <p class="para">
         At Veteran Computer Solutions, we believe that technology should enhance your life, not be frustrating. Our team of experienced IT professionals is dedicated to helping you get the most from your technology. We are committed to providing quality IT support to all businesses, especially small and medium-sized enterprises that often face unique challenges. Our mission is to make your company stand out by leveraging the power of technology to help you achieve your business goals.        </p>
+    <br>
+        The owner of Veteran Compuer Solutions has over 30 years of experience from being a tech to an IT Director.  He specializes in being able to communicate in a language that everyone understands.
     </div>
     <div class="grey-box2">
         <h4 class="page-header">Our Vision</h4>
