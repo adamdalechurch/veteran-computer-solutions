bdc4735b646cef227a5235a61362238a24065dc5
garylstarr <155030944+garylstarr@users.noreply.github.com>
2024-01-26 10:48:56 -0500

    Update about.php
    about owner

--- pages/about.php
+++ pages/about.php
@@ -14,6 +14,16 @@
         <p class="para">
             At Veteran Computer Solutions, we believe that technology should enhance your life, not be frustrating. Our team of experienced IT professionals is dedicated to helping you get the most from your technology. We are committed to providing quality IT support to all businesses, especially small and medium-sized enterprises that often face unique challenges. Our mission is to make your company stand out by leveraging the power of technology to help you achieve your business goals.        </p>
     </div>
+
+     <div class="grey-box3">
+        <h4 class="page-header">Our Owner</h4>
+        <div class="separator"></div>
+        <p class="para">
+            Gary Starr is the owner of Veteran Computer Solutions.  He has over 30 years of IT experience ranging from starting as a beginner computer tech to an IT Director for 11 years. Gary has worked in various small and medium business sectors and understands that technology needs to be customized to each business. During the past 30 years, Gary has led several IT projects which include large upgrades and rollouts of new computers, servers, networks, websites, creating a new IT department & systems for a new company, and business operations.  He prides himself in being able to communicate effectively to each customer and stakeholder at their level so that they understand and embrace technology to its fullest.
+        <br>
+            In addition to being a seasoned IT professional, Gary is married to his beautiful wife and has 3 adult children and 1 grandson.  They also have 5 fur babies which include 3 dogs and 2 cats which all have Disney names.  Gary and his wife enjoy spending time at Disney, antiquing, and exploring Florida. He enjoys running and have participated in many races such as the Skyway 10K, Army Ten-Miler, and has completed the Dopey Challenge at Disney 4 times so far.  Gary is also a proud veteran of the US Army.
+        </p>
+    </div>
     <div class="separator2"></div>
 </div>
 
